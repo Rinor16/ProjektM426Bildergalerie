9a4d53c24c30f305102a95a98fee83f2bdbb691e
Besart Jashari <116434386+besarti96@users.noreply.github.com>
2023-05-13 16:36:46 +0200

    Add files via upload

--- BilderGallerie/index.php
+++ BilderGallerie/index.php
@@ -5,106 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Bildergalerie</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            // Funktion zum Aktualisieren der Bildergalerie
-            function updateGallery() {
-                $.ajax({
-                    url: 'get_gallery.php', // Datei zum Abrufen der Bildergalerie-Daten
-                    type: 'GET',
-                    success: function(response) {
-                        $('#bildergalerie').html(response); // Aktualisiere den Inhalt der Bildergalerie
-                    },
-                    error: function() {
-                        alert('Fehler beim Abrufen der Bildergalerie.');
-                    }
-                });
-            }
-
-            // Rufe die Bildergalerie beim Laden der Seite auf
-            updateGallery();
-
-            // Handler für das Hochladen des Bildes
-            $('#upload-form').submit(function(event) {
-                event.preventDefault(); // Verhindere das Standardverhalten des Formulars
-
-                var formData = new FormData(this);
-
-                $.ajax({
-                    url: 'upload_image.php', // Datei zum Hochladen des Bildes
-                    type: 'POST',
-                    data: formData,
-                    processData: false,
-                    contentType: false,
-                    success: function(response) {
-                        updateGallery(); // Aktualisiere die Bildergalerie nach dem Hochladen
-                        $('#upload-form')[0].reset(); // Zurücksetzen des Formulars
-                    },
-                    error: function() {
-                        alert('Fehler beim Hochladen des Bildes.');
-                    }
-                });
-            });
-        });
-    </script>
 </head>
 <body>
     <h1>Bildergalerie</h1>
 
-    <!-- Formular zum Hochladen des Bildes -->
-    <form id="upload-form" method="POST" enctype="multipart/form-data">
+    <?php
+    session_start();
+    require 'functions.php';
+
+    // Datenbankverbindung herstellen
+    $conn = establishConnection();
+
+    // Datenbank und Tabellen erstellen, falls sie nicht existieren
+    createDatabaseIfNotExists($conn);
+    createAbteilungTableIfNotExists($conn);
+    createAuftraggeberTableIfNotExists($conn);
+    createMitarbeiterTableIfNotExists($conn);
+    createProjektTableIfNotExists($conn);
+    createLoginTableIfNotExists($conn);
+
+    // Bild hinzufügen und in die Datenbank hochladen
+    if (isset($_POST['upload'])) {
+        $uploadedImage = $_FILES['image']['name'];
+        uploadImage($conn, $uploadedImage);
+        echo "Bild erfolgreich hochgeladen.";
+    }
+    ?>
+
+    <!-- Bild hochladen -->
+    <form method="POST" enctype="multipart/form-data">
         <input type="file" name="image" required>
-        <button type="submit">Bild hochladen</button>
+        <input type="submit" name="upload" value="Bild hochladen">
     </form>
 
-    <!-- Bildergalerie -->
-    <div id="bildergalerie">
-        <!-- Hier wird die Bildergalerie dynamisch aktualisiert -->
-    </div>
+    <!-- Weitere HTML-Elemente hier ... -->
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            // Funktion zum Aktualisieren der Bildergalerie
-            function updateGallery() {
-                $.ajax({
-                    url: 'get_gallery.php', // Datei zum Abrufen der Bildergalerie-Daten
-                    type: 'GET',
-                    success: function(response) {
-                        $('#bildergalerie').html(response); // Aktualisiere den Inhalt der Bildergalerie
-                    },
-                    error: function() {
-                        alert('Fehler beim Abrufen der Bildergalerie.');
-                    }
-                });
-            }
+    <?php
+    // Datenbankverbindung schließen
+    closeConnection($conn);
+    ?>
 
-            // Rufe die Bildergalerie beim Laden der Seite auf
-            updateGallery();
-
-            // Handler für das Hochladen des Bildes
-            $('#upload-form').submit(function(event) {
-                event.preventDefault(); // Verhindere das Standardverhalten des Formulars
-
-                var formData = new FormData(this);
-
-                $.ajax({
-                    url: 'upload_image.php', // Datei zum Hochladen des Bildes
-                    type: 'POST',
-                    data: formData,
-                    processData: false,
-                    contentType: false,
-                    success: function(response) {
-                        updateGallery(); // Aktualisiere die Bildergalerie nach dem Hochladen
-                        $('#upload-form')[0].reset(); // Zurücksetzen des Formulars
-                    },
-                    error: function() {
-                        alert('Fehler beim Hochladen des Bildes.');
-                    }
-                });
-            });
-        });
-    </script>
 </body>
 </html>
